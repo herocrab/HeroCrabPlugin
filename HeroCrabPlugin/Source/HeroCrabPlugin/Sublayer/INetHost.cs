c46bdd2e6c486e46300c273ac4f1667f143f0de9
herocrab <jarmo@herocrab.com>
2021-07-01 10:18:11 -0600

    Simplified banner.

--- HeroCrabPlugin/Source/HeroCrabPlugin/Sublayer/INetHost.cs
+++ HeroCrabPlugin/Source/HeroCrabPlugin/Sublayer/INetHost.cs
@@ -1,5 +1,4 @@
-﻿/* Copyright (c) Jeremy Buck "Jarmo" - HeroCrab Ltd. (https://github.com/herocrab)
-Distributed under the MIT license. See the LICENSE.md file in the project root for more information. */
+﻿// Copyright (c) Jeremy Buck "Jarmo" - HeroCrab Ltd. (https://github.com/herocrab). Distributed under the MIT license.
 using System;
 
 // ReSharper disable UnusedMemberInSuper.Global
--- HeroCrabPlugin/Source/HeroCrabPluginTestsUnit/Field/NetFIeldDescTests.cs
+++ HeroCrabPlugin/Source/HeroCrabPluginTestsUnit/Field/NetFIeldDescTests.cs
@@ -1,5 +1,4 @@
-﻿/* Copyright (c) Jeremy Buck "Jarmo" - HeroCrab Ltd. (https://github.com/herocrab)
-Distributed under the MIT license. See the LICENSE.md file in the project root for more information. */
+﻿// Copyright (c) Jeremy Buck "Jarmo" - HeroCrab Ltd. (https://github.com/herocrab). Distributed under the MIT license.
 using HeroCrabPlugin.Core;
 using HeroCrabPlugin.Element;
 using HeroCrabPlugin.Field;
